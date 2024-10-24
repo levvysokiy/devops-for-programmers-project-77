30c83e3eccd8f472d6e953934e97a39137cef059
levvysokiy <lev@lexense.com>
2024-10-24 19:31:32 +0300

    domain and cert

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,24 +37,55 @@ resource "digitalocean_droplet" "web2" {
 resource "digitalocean_loadbalancer" "web_lb" {
   name   = "example-lb"
   region = var.region
+  forwarding_rule {
+    entry_port      = 80
+    entry_protocol  = "http"
+    target_port     = 80
+    target_protocol = "http"
+  }
 
   forwarding_rule {
-    entry_protocol  = "http"
-    entry_port      = 80
-    target_protocol = "http"
-    target_port     = 80
+    entry_port       = 443
+    entry_protocol   = "https"
+    target_port      = 80
+    target_protocol  = "http"
+    certificate_name = digitalocean_certificate.cert.name
+    tls_passthrough  = false
   }
 
   healthcheck {
-    protocol                 = "http"
-    port                     = 80
-    path                     = "/"
-    check_interval_seconds   = 10
-    response_timeout_seconds = 5
-    healthy_threshold        = 5
-    unhealthy_threshold      = 3
+    protocol = "http"
+    port     = 80
+    path     = "/"
   }
 
   droplet_ids = [digitalocean_droplet.web1.id, digitalocean_droplet.web2.id]
   depends_on  = [digitalocean_droplet.web1, digitalocean_droplet.web2]
+}
+
+# Add a domain to DigitalOcean
+resource "digitalocean_domain" "web_domain" {
+  name = "vysokiylev.xyz"
+}
+
+resource "digitalocean_record" "www" {
+  domain = digitalocean_domain.web_domain.name
+  type   = "A"
+  name   = "www"
+  value  = digitalocean_loadbalancer.web_lb.ip
+}
+
+resource "digitalocean_record" "root" {
+  domain = digitalocean_domain.web_domain.name
+  type   = "A"
+  name   = "@"
+  value  = digitalocean_loadbalancer.web_lb.ip
+}
+
+resource "digitalocean_certificate" "cert" {
+  type    = "lets_encrypt"
+  name    = "web-cert"
+  domains = [digitalocean_domain.web_domain.name]
+
+  depends_on = [digitalocean_domain.web_domain]
 }
