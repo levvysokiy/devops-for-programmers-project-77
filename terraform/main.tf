b449f2e20d288240e804e2d06ba003835c4a031f
levvysokiy <lev@lexense.com>
2024-10-24 18:01:00 +0300

    second step

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# Create a new Droplet (VM)
+resource "digitalocean_droplet" "web1" {
+  name   = "${var.droplet_name}-1"
+  region = var.region
+  size   = var.size
+  image  = var.image
+  tags   = var.tags
+
+  # Optional: Configure SSH key
+  ssh_keys = [var.ssh_fingerprint]
+}
+
+resource "digitalocean_droplet" "web2" {
+  name   = "${var.droplet_name}-2"
+  region = var.region
+  size   = var.size
+  image  = var.image
+  tags   = var.tags
+
+  # Optional: Configure SSH key
+  ssh_keys = [var.ssh_fingerprint]
+}
+
+resource "digitalocean_loadbalancer" "web_lb" {
+  name   = "example-lb"
+  region = var.region
+
+  forwarding_rule {
+    entry_protocol  = "http"
+    entry_port      = 80
+    target_protocol = "http"
+    target_port     = 80
+  }
+
+  healthcheck {
+    protocol                 = "http"
+    port                     = 80
+    path                     = "/"
+    check_interval_seconds   = 10
+    response_timeout_seconds = 5
+    healthy_threshold        = 5
+    unhealthy_threshold      = 3
+  }
+
+  droplet_ids = [digitalocean_droplet.web1.id, digitalocean_droplet.web2.id]
+  depends_on  = [digitalocean_droplet.web1, digitalocean_droplet.web2]
+}
