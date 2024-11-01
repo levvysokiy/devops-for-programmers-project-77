c2434e3c774d4e43121d821ae5c1f38429f0d5ae
levvysokiy <lev@lexense.com>
2024-11-01 22:15:09 +0200

    moved provider settings to another file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,3 @@
-terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-    datadog = {
-      source  = "datadog/datadog"
-      version = "~> 3.0" # specify the version or use latest compatible version
-    }
-  }
-}
-
-provider "digitalocean" {
-  token = var.do_token
-}
-
-provider "datadog" {
-  api_key = var.datadog_api_key
-  app_key = var.datadog_app_key
-  api_url = "https://api.us5.datadoghq.com/"
-}
-
 # Create a new Droplet (VM)
 resource "digitalocean_droplet" "web1" {
   name   = "${var.droplet_name}-1"
